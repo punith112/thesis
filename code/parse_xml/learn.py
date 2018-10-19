b4a6b0cbaae7869b4b6cba802d87f9df58a38766
Sashidhar Kanuboddi <sashi2295@gmail.com>
2018-10-19 20:28:15 +0200

    plotted GMM for x-coord of mouse

--- code/parse_xml/learn.py
+++ code/parse_xml/learn.py
@@ -1,25 +1,25 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn import mixture
 
-my_data = np.genfromtxt('mouse_features.csv', delimiter = ',')
-#
-# np.random.seed(444)
-# np.set_printoptions(precision=3)
-#
-# d = np.random.laplace(loc=15, scale=3, size=500)
-#
-# # hist, bin_edges = np.histogram(my_data[:,0])
-#
-n, bins, patches = plt.hist(my_data[:,0], bins=200)
+my_data = np.genfromtxt('mouse_features.csv', delimiter=',')
+
+X = my_data[:, 0].reshape(-1, 1)
+
+# n, bins, patches = plt.hist(X, bins=100)
 # plt.grid(axis='y', alpha=0.75)
 # plt.xlabel('Value')
-plt.ylabel('Frequency')
+# plt.ylabel('Frequency')
 # plt.title('My Very Own Histogram')
 # plt.xlim(-1000505, 5)
 # plt.show()
 
-# x = np.random.random_integers(1, 100000, 10000)
-# x = [1, 2, 2, 3, 4, 5, 6, 7, 8, 10]
-# n, bins, patches = plt.hist(x, bins=9, rwidth=0.5)
-# plt.ylabel('No of times')
-# plt.show()
+clf = mixture.GaussianMixture(n_components=3, covariance_type="full")
+clf.fit(X)
+
+X_test = np.linspace(-3, 3, 400).reshape(-1, 1)
+
+log_prob = clf.score_samples(X_test)
+pdf = np.exp(log_prob)
+plt.plot(X_test, pdf)
+plt.show()

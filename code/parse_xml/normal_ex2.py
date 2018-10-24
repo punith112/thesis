bb7cf57d7011d2029d20366c8c969307838e8e79
Sashidhar Kanuboddi <sashi2295@gmail.com>
2018-10-24 02:33:59 +0200

    continuing to play around with 1D gaussian mixtures

--- code/parse_xml/normal_ex2.py
+++ code/parse_xml/normal_ex2.py
@@ -1,11 +1,7 @@
-# An example in which data is generated from two different
-# normal distributions and then later, a single normal distribution
-# is fitted to this data. As expected, the final distribution that is
-# fitted is a bad fit to the data. A mixture of distributions is a better option.
-
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy.stats import norm
+from sklearn import mixture
 
 # Generating random data using a normal distribution
 mu_one = 0
@@ -18,17 +14,16 @@ data_one = np.random.normal(mu_one, sigma_one, 1000)
 data_two = np.random.normal(mu_two, sigma_two, 1000)
 
 data = np.concatenate((data_one, data_two), axis=0)
+data = data.reshape(-1, 1)
 
 # Plotting a normalised histogram of the data (bins are 10 by default)
 count, bins, patches = plt.hist(data, alpha=0.6, density=True, color='b')
 
-# Using this data to fit a new normal distribution
-mu_new, sigma_new = norm.fit(data)
+clf = mixture.GaussianMixture(n_components=2, covariance_type='full')
+clf.fit(data)
 
-# Plotting the fitted normal distribution
 xmin, xmax = plt.xlim()
 x = np.linspace(xmin, xmax, 100)
-
-
-pdf_new = norm.pdf(x, mu_new, sigma_new)
-plt.plot(x, pdf_new, 'r', linewidth=2)
+x = x.reshape(-1, 1)
+pdf = np.exp(clf.score_samples(x))
+plt.plot(x, pdf, 'r', linewidth=1)

b70a5a59956697acaa9229774782e0f28d2e57e2
Sashidhar Kanuboddi <sashi2295@gmail.com>
2018-11-29 17:21:56 +0100

    saving single object features to file

--- code/learn/iki/single_obj_features.py
+++ code/learn/iki/single_obj_features.py
@@ -102,5 +102,8 @@ gmm_df = pd.DataFrame()
 
 for obj in objects_in_scene:
     feature_set = extract_single_obj_features(main_df, obj, features)
+    filename = obj + '_' + 'features'
+    main_df.to_csv(filename, sep = '\t')
+    
     param_series = fit_gmm(obj, feature_set)
     gmm_df[obj] = param_series

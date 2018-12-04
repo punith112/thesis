2c9854654d461a572c7f19dc0c253a65c4c97b02
Sashidhar Kanuboddi <sashi2295@gmail.com>
2018-12-04 18:58:13 +0100

    testing the class implementations

--- code/learn/iki/test.py
+++ code/learn/iki/test.py
@@ -1,5 +1,10 @@
-from single_object import SingleObjectFeatures
+# from single_object import SingleObjectFeatures
+#
+# features = ['x', 'y', 'z', 'length', 'width', 'height']
+# test = SingleObjectFeatures("extracted_data.txt", "database", features)
+# result = test.get_gmm_params()
 
-features = ['x', 'y', 'z', 'length', 'width', 'height']
-test = SingleObjectFeatures("extracted_data.txt", "database", features)
+from object_pair import ObjectPairFeatures
+
+test = ObjectPairFeatures("extracted_data.txt")
 result = test.get_gmm_params()

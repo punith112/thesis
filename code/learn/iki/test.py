0574df31c625b389692bd28afa1759d87739e2de
Sashidhar Kanuboddi <sashi2295@gmail.com>
2018-12-04 19:21:02 +0100

    testing class implementations

--- code/learn/iki/test.py
+++ code/learn/iki/test.py
@@ -1,10 +1,21 @@
-# from single_object import SingleObjectFeatures
-#
-# features = ['x', 'y', 'z', 'length', 'width', 'height']
-# test = SingleObjectFeatures("extracted_data.txt", "database", features)
-# result = test.get_gmm_params()
+from single_object import SingleObjectWrapper
+from object_pair import ObjectPairWrapper
+import pickle
 
-from object_pair import ObjectPairFeatures
+with open("extracted_data.txt", "rb") as myFile:
+    main_list = pickle.load(myFile)
 
-test = ObjectPairFeatures("extracted_data.txt")
-result = test.get_gmm_params()
+objects_in_scene = []
+
+for i in range(len(main_list)):
+    objects_in_scene = list(set().union(objects_in_scene, main_list[i].keys()))
+
+objects_in_scene.remove('file')
+
+features = ['x', 'y', 'z', 'length', 'width', 'height']
+
+single_object_wrapper = SingleObjectWrapper(objects_in_scene, "database", features)
+single_object_gmms = single_object_wrapper.get_gmm_params()
+
+object_pair_wrapper = ObjectPairWrapper(objects_in_scene)
+object_pair_gmms = object_pair_wrapper.get_gmm_params()
